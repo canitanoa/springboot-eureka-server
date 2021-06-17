98f45db987588b36be94b20ef8c5243ce582630f
Adrián Canitano <acanitano@gmail.com>
2021-06-17 15:55:18 -0300

    Config EurekaSerever

--- src/main/java/ar/com/springboot/ms/eureka/SpringbootServicioEurekaServerApplication.java
+++ src/main/java/ar/com/springboot/ms/eureka/SpringbootServicioEurekaServerApplication.java
@@ -2,7 +2,9 @@ package ar.com.springboot.ms.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringbootServicioEurekaServerApplication {
 
